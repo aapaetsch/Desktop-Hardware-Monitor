bb26784aac332a9ca7db72fe2195867d538e1a78
Aidan Paetsch <aapaetsch@gmail.com>
2022-12-22 00:53:03 -0700

    Initial commit, application built with electron-forge
    - Working electron-typescript-C# project
    
    - Connection between electron and .net is working
    
    - React still needs to be added

---
+++ DesktopHardwareMonitorService/LibreHardwareMonitorService/Program.cs
@@ -0,0 +1,24 @@
+﻿// using System;
+using ElectronCgi.DotNet;
+using Microsoft.Extensions.Logging;
+
+namespace LibreHardwareMonitorService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var connection = new ConnectionBuilder()
+                .WithLogging(minimumLogLevel: LogLevel.Trace, logFilePath: "the-log-file.txt")
+                .Build();
+            
+            connection.On<string, string>("greeting", name => {
+                var response = $"Hello {name}";
+                return response;
+            });
+            
+            connection.Listen();    
+            //connection.OnAsync();
+        }
+    }
+}
