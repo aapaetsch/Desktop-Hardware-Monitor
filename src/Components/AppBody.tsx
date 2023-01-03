52016b6320f7544ec46d75a41e1ca7cb607f9473
Aidan Paetsch <aapaetsch@gmail.com>
2023-01-03 01:20:48 -0700

    Demo horizontal stat indicator in appbody

--- src/Components/AppBody.tsx
+++ src/Components/AppBody.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React from "react";
+import HorizontalStatIndicator from "./StatIndicators/HorizontalStatIndicator/HorizontalStatIndicator";
+import { FrequencyUnits } from "./StatIndicators/statIndicatorTypes";
 
 const AppBody: React.FC = () => {
-    
-    return <div id="appBody" className="background-main">
-        </div>;
+    const [computerStat1, setComputerStat1] = React.useState(4800);
+    setTimeout(() => {
+        if (computerStat1 === 0) {
+            setComputerStat1(4800);
+        } else {
+            setComputerStat1(computerStat1 - 100);
+        }
+    }, 1000);
+    return (
+        <div id="appBody" className="background-main">
+            <HorizontalStatIndicator
+                currentValue={computerStat1}
+                minValue={0}
+                maxValue={4800}
+                activeColorClass="is-primary"
+                width={window.innerWidth / 3}
+                labelLeft={{ value: "Core #1", units: null }}
+                labelRight={{
+                    value: computerStat1,
+                    units: FrequencyUnits.MegaHertz,
+                }}
+                hasLabelsAbove={false}
+            />
+        </div>
+    );
 };
 export default AppBody;
